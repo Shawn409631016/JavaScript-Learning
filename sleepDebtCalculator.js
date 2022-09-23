de023c63ab87452e5969574f140c45e86ce5fdf8
shawn <shawnfeng610@gmail.com>
2022-09-24 00:23:19 +0800

    format

--- sleepDebtCalculator.js
+++ sleepDebtCalculator.js
@@ -1,29 +1,29 @@
 const getSleepHours = (day) => {
     switch (day) {
-      case "Monday":
-        return 8;
-        break;
-      case "Tuesday":
-        return 8;
-        break;
-      case "Wednesday":
-        return 8;
-        break;
-      case "Thursday":
-        return 8;
-        break;
-      case "friday":
-        return 8;
-        break;
-      case "Saturday":
-        return 12;
-        break;
-      case "Sunday":
-        return 12;
-        break;
+        case "Monday":
+            return 8;
+            break;
+        case "Tuesday":
+            return 8;
+            break;
+        case "Wednesday":
+            return 8;
+            break;
+        case "Thursday":
+            return 8;
+            break;
+        case "friday":
+            return 8;
+            break;
+        case "Saturday":
+            return 12;
+            break;
+        case "Sunday":
+            return 12;
+            break;
     }
-  };
-  const getActualSleepHours = () =>
+};
+const getActualSleepHours = () =>
     getSleepHours("Monday") +
     getSleepHours("Tuesday") +
     getSleepHours("Wednesday") +
@@ -31,19 +31,19 @@ const getSleepHours = (day) => {
     getSleepHours("Friday") +
     getSleepHours("Saturday") +
     getSleepHours("Sunday");
-  const getIdealSleepHours = () => {
+const getIdealSleepHours = () => {
     const idealHours = 9;
     return idealHours * 7;
-  };
-  const calculateSleepDebt = () => {
+};
+const calculateSleepDebt = () => {
     const actualSleepHours = getActualSleepHours();
     const idealSleepHours = getIdealSleepHours();
     if (actualSleepHours > idealSleepHours) {
-      console.log("You got more sleep than needed.");
+        console.log("You got more sleep than needed.");
     } else if (actualSleepHours < idealSleepHours) {
-      console.log("You should get some rest.");
+        console.log("You should get some rest.");
     } else {
-      console.log("You got the perfect amount of sleep.");
+        console.log("You got the perfect amount of sleep.");
     }
-  };
-  calculateSleepDebt();
+};
+calculateSleepDebt();

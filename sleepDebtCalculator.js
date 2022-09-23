5e976e2680c6ed26b9d632724fffb44e0f335985
shawn <shawnfeng610@gmail.com>
2022-09-24 00:03:15 +0800

    add Sleep Debt Calculator

---
+++ sleepDebtCalculator.js
@@ -0,0 +1,49 @@
+const getSleepHours = (day) => {
+    switch (day) {
+      case "Monday":
+        return 8;
+        break;
+      case "Tuesday":
+        return 8;
+        break;
+      case "Wednesday":
+        return 8;
+        break;
+      case "Thursday":
+        return 8;
+        break;
+      case "friday":
+        return 8;
+        break;
+      case "Saturday":
+        return 12;
+        break;
+      case "Sunday":
+        return 12;
+        break;
+    }
+  };
+  const getActualSleepHours = () =>
+    getSleepHours("Monday") +
+    getSleepHours("Tuesday") +
+    getSleepHours("Wednesday") +
+    getSleepHours("Thursday") +
+    getSleepHours("Friday") +
+    getSleepHours("Saturday") +
+    getSleepHours("Sunday");
+  const getIdealSleepHours = () => {
+    const idealHours = 9;
+    return idealHours * 7;
+  };
+  const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours();
+    if (actualSleepHours > idealSleepHours) {
+      console.log("You got more sleep than needed.");
+    } else if (actualSleepHours < idealSleepHours) {
+      console.log("You should get some rest.");
+    } else {
+      console.log("You got the perfect amount of sleep.");
+    }
+  };
+  calculateSleepDebt();
